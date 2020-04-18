9ddeb2d865d001b6438cea2cf7289be8a7b38ca5
esimony <eden.shi88@gmail.com>
2020-04-18 23:50:19 +0300

    fix map and change view

--- FlightSimulatorApp/Views/View.xaml.cs
+++ FlightSimulatorApp/Views/View.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using FlightSimulatorApp.Models;
 using FlightSimulatorApp.ViewModels;
+using System;
 using System.Windows;
 
 namespace FlightSimulatorApp.Views
@@ -18,64 +19,7 @@ namespace FlightSimulatorApp.Views
 
         public View(string port, string ip)
         {
-            // Connect settings  - client and server.
-            telnetClient = new MyTelnetClient();
-            Application.Current.Properties["model"] = new MySimulatorModel(telnetClient);
-            mySimulator = (MySimulatorModel)Application.Current.Properties["model"];
-
-            // Define all controls in the view window.
-            joystickViewModel = new JoystickViewModel(mySimulator);
-            dashboardViewModel = new DashboardViewModel(mySimulator);
-            errorViewModel = new ErrorViewModel(mySimulator);
-            mapViewModel = new MapViewModel((MySimulatorModel)Application.Current.Properties["model"]);
-            if (mySimulator.connect(ip, int.Parse(port)) == 0)
-                mySimulator.start();
-
-            InitializeComponent();
-            Joystick.DataContext = joystickViewModel;
-            dash.DataContext = dashboardViewModel;
-            error.DataContext = errorViewModel;
-            map.DataContext = mapViewModel;
-            disconnect.DataContext = mapViewModel;
-        }
-
-        private void disconnect_Click(object sender, RoutedEventArgs e)
-        {
-            // Check if server is connect.
-            if (mapViewModel.Connect)
-            {
-                telnetClient.disconnect();
-            }
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
-            this.Close();
-        }
-    }
-}
-
-/*
- // maybe delete this!!!!
- // maybe we can change to this kod
- using FlightSimulatorApp.Models;
-using FlightSimulatorApp.ViewModels;
-using System.Windows;
-
-namespace FlightSimulatorApp.Views
-{
-    /// <summary>
-    /// Interaction logic for View.xaml
-    /// </summary>
-    public partial class View : Window
-    {
-        private MyTelnetClient telnetClient;
-        private MySimulatorModel mySimulator;
-        private JoystickViewModel joystickViewModel;
-        private DashboardViewModel dashboardViewModel;
-        private MapViewModel mapViewModel;
-        private ErrorViewModel errorViewModel;
-
-        public View(string port, string ip)
-        {
+            int tempPort;
             // Connect settings  - client and server.
             telnetClient = new MyTelnetClient();
             Application.Current.Properties["model"] = new MySimulatorModel(telnetClient);
@@ -86,7 +30,17 @@ namespace FlightSimulatorApp.Views
             dashboardViewModel = new DashboardViewModel(mySimulator);
             errorViewModel = new ErrorViewModel(mySimulator);
             mapViewModel = new MapViewModel(mySimulator);
-            if (mySimulator.connect(ip, int.Parse(port)) == 0)
+            // Checks the reseve port.
+            try
+            {
+                tempPort = int.Parse(port);
+            }
+            catch
+            {
+                Console.WriteLine("Error - worng formt of port");
+                tempPort = 0;
+            }
+            if (mySimulator.connect(ip, tempPort) == 0)
                 mySimulator.start();
 
             InitializeComponent();
@@ -110,5 +64,3 @@ namespace FlightSimulatorApp.Views
         }
     }
 }
-
- */
